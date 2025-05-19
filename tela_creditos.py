03f4e99861a1aa35ab194f2cabd0089e91471154
jfleury07 <julia.nigrin@gmail.com>
2025-05-19 16:43:33 -0300

    alt

--- tela_creditos.py
+++ tela_creditos.py
@@ -7,4 +7,15 @@ def desenha_texto (texto, fonte, cor, superficie, x, y):
     texto_rect.center = (x, y)
     superficie.blit (texto_obj, texto_rect)
 
+def tela_de_creditos (tela, status): 
+    BRANCO = (255, 255, 255)
+    PRETO = (0, 0, 0)
+
+    fonte_titulo = pygame.font.Font ("fontes/fonte1.ttf", 100)
+    fonte_texto = pygame.font.Font ("fontes/fonte3.ttf", 26)
+
+    tela.fill (88, 110, 124)
+    mouse_x = pygame.mouse.get_pos()
+    mouse_y = pygame.mouse.get_pos()
+
     
