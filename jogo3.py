46f13c4202f0f5f09074f4d3fb5aacb02f73d2d9
jfleury07 <julia.nigrin@gmail.com>
2025-05-19 11:55:38 -0300

    classe Game ()

--- jogo3.py
+++ jogo3.py
@@ -12,3 +12,26 @@ correr = True
 
 dt = 0 
 pos_jogador = pygame.Vector2(tela.get_width()/2, tela.get_height())
+
+# Definindo uma classe do game
+class Game(): 
+    def __init__(self, grupo_aspen, grupo_comida): 
+        self.grupo_aspen = grupo_aspen
+        self.grupo_comida = grupo_comida
+        self.pontos = 0
+        self.vidas = 3
+
+        # Definir a fonte
+        self.fonte_pequena = pygame.font.Sysfont ("imapct", 24)
+        self.fonte_grande = pygame.font.Sysfont ("imapct", 60)
+
+        # Definir as imagens das "comidas"
+        python_azul = pygame.image.load ("images/food.png")
+        python_vermelho = pygame.image.load ("images/chave.png")
+
+        # Adicionar as "comidas" pro grupo de comidas
+        # Sendo o python_azul = 0 e o python_vermelho = 1
+        self.grupo_comida.add (Food(190, 200, python_vermelho, 1))
+        for i in range (7): 
+            self.food_group.add (Food(200, 200, python_azul, 0))
+            
