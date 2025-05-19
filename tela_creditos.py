2891844238ec9b6e41cfbfeed34a082b930f299c
jfleury07 <julia.nigrin@gmail.com>
2025-05-19 16:41:11 -0300

    tela dos créditos

---
+++ tela_creditos.py
@@ -0,0 +1,10 @@
+import pygame 
+import sys 
+
+def desenha_texto (texto, fonte, cor, superficie, x, y): 
+    texto_obj = fonte.render (texto, True, cor)
+    texto_rect = texto_obj.get_rect ()
+    texto_rect.center = (x, y)
+    superficie.blit (texto_obj, texto_rect)
+
+    
